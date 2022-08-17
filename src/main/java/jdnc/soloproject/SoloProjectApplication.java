f369609a073fa2650d711ff2261bd0ec0328f0ad
Jihyo Kang <luxuryjihyo@naver.com>
2022-08-17 10:29:39 +0900

    first commit

---
+++ src/main/java/jdnc/soloproject/SoloProjectApplication.java
@@ -0,0 +1,13 @@
+package jdnc.soloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectApplication.class, args);
+	}
+
+}
